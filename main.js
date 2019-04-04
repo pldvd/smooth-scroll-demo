8d74ff0218a73861a9b7a4469bdb1d40d78dcd33
David Pal <paldavid2030@gmail.com>
2019-04-04 11:35:43 +0300

    change scrolling for scrollIntoView and adjust css

--- main.js
+++ main.js
@@ -4,41 +4,27 @@ const productsButton = document.querySelector('li.products');
 const aboutButton = document.querySelector('li.about');
 
 aboutButton.addEventListener('click', () => {
-  smoothScroll('#about', 700);
+  const aboutPage = document.getElementById('about');
+  aboutPage.scrollIntoView({
+    behavior: 'smooth'
+  })
 })
 
 contactButton.addEventListener('click', () => {
-  smoothScroll('#contact', 700);
+  document.getElementById('contact').scrollIntoView({
+    behavior: 'smooth'
+  })
+
 })
 
 technologyButton.addEventListener('click', () => {
-  smoothScroll('#technology', 700);
+  document.getElementById('technology').scrollIntoView({
+    behavior: 'smooth'
+  })
 })
 
 productsButton.addEventListener('click', () => {
-  smoothScroll('#products', 700);
+  document.getElementById('products').scrollIntoView({
+    behavior: 'smooth'
+  })
 })
-
-function smoothScroll(target, duration) {
-  const targetDiv = document.querySelector(target);
-  const targetDivPosition = targetDiv.getBoundingClientRect().top;
-  const startPosition = window.pageYOffset;
-  let startTime = null;
-
-  function animation(currentTime) {
-    startTime === null ? startTime = currentTime : startTime;
-    const timeElapsed = currentTime - startTime;
-    const run = ease(timeElapsed, startPosition, targetDivPosition, duration);
-    window.scrollTo(0, run);
-    if (timeElapsed < duration) requestAnimationFrame(animation);
-  }
-
-  function ease(t, b, c, d) {
-    t /= d / 2;
-    if (t < 1) return c / 2 * t * t * t + b;
-    t -= 2;
-    return c / 2 * (t * t * t + 2) + b;
-  };
-
-  requestAnimationFrame(animation)
-}
