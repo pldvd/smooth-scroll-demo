b737a7c10e092a7ac6f954e733d075d88fb26e35
David Pal <paldavid2030@gmail.com>
2019-04-05 11:17:18 +0300

    works in firefox but not in chrome :poop:

--- main.js
+++ main.js
@@ -13,6 +13,38 @@ function jumpToPage(pageId) {
   })
 }
 
+function jumpToNext() {
+  let pageId = '';
+
+  if (0 <= currentScrollY && currentScrollY < window.innerHeight) {
+    pageId = 'products';
+  } else if (window.innerHeight <= currentScrollY && currentScrollY < (2 * window.innerHeight)) {
+    pageId = 'technology';
+  } else if ((2 * window.innerHeight) <= currentScrollY) {
+    pageId = 'contact';
+  }
+
+  jumpToPage(pageId);
+}
+
+function jumpToPrevious() {
+  let pageId = '';
+
+  if (0 <= currentScrollY && currentScrollY < window.innerHeight) {
+    pageId = 'about';
+  } else if (window.innerHeight <= currentScrollY && currentScrollY < (2 * window.innerHeight)) {
+    pageId = 'products';
+  } else if ((2 * window.innerHeight) <= currentScrollY) {
+    pageId = 'technology';
+  }
+
+  jumpToPage(pageId);
+}
+
+document.addEventListener('scroll', () => {
+  currentScrollY = window.scrollY;
+});
+
 document.addEventListener('wheel', (e) => {
   if (e.deltaY > 0) {
     jumpToNext();
